6a07f8deff6812209f5037a74ff7e3377d1114c9
hasnain shah <hasnainshah1161@gmail.com>
2019-09-27 01:06:25 +0500

    Updates

---
+++ precache-manifest.059181d3abf18fbfe4073633fd385874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877a276db12dc2e4592addb1df0fa74e",
+    "url": "/devices/index.html"
+  },
+  {
+    "revision": "40811096b4a4709a58f4",
+    "url": "/devices/static/css/2.027378fa.chunk.css"
+  },
+  {
+    "revision": "03a5c4705b7cb0a6b538",
+    "url": "/devices/static/css/main.c960ad86.chunk.css"
+  },
+  {
+    "revision": "40811096b4a4709a58f4",
+    "url": "/devices/static/js/2.6cc4852c.chunk.js"
+  },
+  {
+    "revision": "03a5c4705b7cb0a6b538",
+    "url": "/devices/static/js/main.074f2dc1.chunk.js"
+  },
+  {
+    "revision": "857e102a1fc7fe0db016",
+    "url": "/devices/static/js/runtime~main.a5286db1.js"
+  }
+]);
